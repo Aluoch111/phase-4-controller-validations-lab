2e9b0bee5af08a06966cc0daf51e70d9db2b8dff
Grace Aluoch <gracedeborah377@gmail.com>
2022-11-28 00:47:27 +0300

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 100}
+    validates :category,inclusion: {within: %w(Fiction Non-Fiction)}
+    
 end
